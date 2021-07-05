863074aa1f7ab9286c7a87aa9f5e2ff65f1c6ba0
Sergey Kozlov <sergei.kozlow@gmail.com>
2021-07-05 10:44:45 +0300

    etcd0: listen on 127.0.0.1 as well.

--- etcd_0.sh
+++ etcd_0.sh
@@ -8,7 +8,7 @@ etcd --data-dir=data.etcd --name ${THIS_NAME} \
 	--initial-advertise-peer-urls http://${THIS_IP}:2380 \
 	--listen-peer-urls http://${THIS_IP}:2380 \
 	--advertise-client-urls http://${THIS_IP}:2379 \
-	--listen-client-urls http://${THIS_IP}:2379 \
+	--listen-client-urls http://127.0.0.1:2379,http://${THIS_IP}:2379 \
 	--initial-cluster ${CLUSTER} \
 	--initial-cluster-state ${CLUSTER_STATE} \
 	--initial-cluster-token ${TOKEN}
